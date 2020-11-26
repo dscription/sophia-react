a07d7839036bfe51b192b9968658214bf0367cc2
dscription <dscriptionplayground@gmail.com>
2020-11-26 15:31:14 -0500

    add topic models

---
+++ models/topic.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const topicSchema = new Schema({
+  name: String,
+  contents: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'Content',
+    },
+  ],
+  attention: { type: Number, default: 0 },
+  streak: Number,
+  goal: String,
+  goalDate: Date,
+  isPublic: { type: Boolean, default: true },
+  isOpen: { type: Boolean, default: true },
+});
+
+module.exports = mongoose.model('Topic', topicSchema);
