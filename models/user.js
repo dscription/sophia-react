08e10d1a14b9c76b9f1dae7c4ebf57c7d09c1ee2
dscription <dscriptionplayground@gmail.com>
2020-11-26 15:27:39 -0500

    update user model

--- models/user.js
+++ models/user.js
@@ -1,28 +1,42 @@
 const mongoose = require('mongoose');
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcrypt');
+const Schema = mongoose.Schema;
 
 const SALT_ROUNDS = 6;
 
-const userSchema = new mongoose.Schema({
-  name: String,
-  email: {type: String, required: true, lowercase: true, unique: true},
-  password: String
-}, {
-  timestamps: true
-});
+const userSchema = new Schema(
+  {
+    name: String,
+    email: { type: String, required: true, lowercase: true, unique: true },
+    alias: String,
+    bio: String,
+    avatar: String,
+
+    password: String,
+    topics: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Topic',
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userSchema.set('toJSON', {
-  transform: function(doc, ret) {
+  transform: function (doc, ret) {
     // remove the password property when serializing doc to JSON
     delete ret.password;
     return ret;
-  }
+  },
 });
 
-userSchema.pre("save", function (next) {
+userSchema.pre('save', function (next) {
   // this will be set to the current document
   const user = this;
-  if (!user.isModified("password")) return next();
+  if (!user.isModified('password')) return next();
   // password has been changed - salt and hash it
   bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
     if (err) return next(err);
