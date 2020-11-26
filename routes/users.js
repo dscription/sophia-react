0565d57478087863bed7819bb9b1619ab2ea419f
dscription <dscriptionplayground@gmail.com>
2020-11-26 14:49:05 -0500

    add initial routes

---
+++ routes/users.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const usersCtrl = require('../controllers/users');
+
+const dan = 'dan'
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(require("../config/auth"));
+router.get("/", checkAuth, usersCtrl.index);
+
+
+/*---------- Auth Checker ----------*/
+function checkAuth(req, res, next) {
+  if (req.user) return next();
+  return res.status(401).json({msg: 'Not Authorized'});
+}
+
+module.exports = router;
