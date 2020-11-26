a98c5628d9da071d69214175eeee99f96dba63df
dscription <dscriptionplayground@gmail.com>
2020-11-26 16:11:29 -0500

    install react-bootstrap

--- src/pages/Onboarding/Onboarding.jsx
+++ src/pages/Onboarding/Onboarding.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import InputGroup from 'react-bootstrap/InputGroup'
 
 class Onboarding extends Component {
-  state = {  }
-  render() { 
-    return ( <h1>Onboarding Page!</h1> );
+  state = {};
+  render() {
+    return (
+      <>
+        <h1>Onboarding Page!</h1>
+        <h2>Welcome! Tell us about all the things you would like to learn!</h2>
+        {/* // TODO: Add input for custom topic */}
+        {/* // TODO: Add pre-defined topics */}
+        <button>Done For Now</button>
+      </>
+    );
   }
 }
- 
+
 export default Onboarding;
