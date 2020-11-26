22d0256b089373c9193ee7a2fae0771be0812431
dscription <dscriptionplayground@gmail.com>
2020-11-26 15:44:07 -0500

    conditionally render Onboarding Page or Home Page in App.js

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,18 +1,19 @@
-import React, { Component } from "react";
-import { Route, Redirect } from "react-router-dom";
-import NavBar from "../../components/NavBar/NavBar";
-import Signup from "../Signup/Signup";
-import Login from "../Login/Login";
-import authService from "../../services/authService";
-import Users from "../Users/Users";
-import "./App.css";
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import NavBar from '../../components/NavBar/NavBar';
+import Signup from '../Signup/Signup';
+import Login from '../Login/Login';
+import Home from '../Home/Home';
+import Onboarding from '../Onboarding/Onboarding';
+import authService from '../../services/authService';
+import Users from '../Users/Users';
+
+import './App.css';
 
 class App extends Component {
   state = {
     user: authService.getUser(),
   };
-
-
 
   handleLogout = () => {
     authService.logout();
@@ -24,18 +25,15 @@ class App extends Component {
   };
 
   render() {
-    const {user} = this.state
+    const { user } = this.state;
+    console.log(user.topics);
     return (
       <>
         <NavBar user={user} handleLogout={this.handleLogout} />
         <Route
           exact
           path="/"
-          render={() => (
-            <main>
-              <h1>Welcome. This is an authorization template.</h1>
-            </main>
-          )}
+          render={() => (user.topics ? <Home /> : <Onboarding />)}
         />
         <Route
           exact
---
+++ src/pages/Onboarding/Onboarding.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Onboarding extends Component {
+  state = {  }
+  render() { 
+    return ( <h1>Onboarding Page!</h1> );
+  }
+}
+ 
+export default Onboarding;
