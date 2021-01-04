6c822f949a7ae8917f2dba21f4f0df0bcf6ccc2c
dscription <dscriptionplayground@gmail.com>
2021-01-03 19:37:30 -0500

    style singup form

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,17 +1,19 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import authService from "../../services/authService";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import authService from '../../services/authService';
+import { LoginBox } from '../StyledComponents/CustomComponents/CustomComponents';
+import { Form, Button } from 'react-bootstrap';
 
 class SignupForm extends Component {
   state = {
-    name: "",
-    email: "",
-    password: "",
-    passwordConf: "",
+    name: '',
+    email: '',
+    password: '',
+    passwordConf: '',
   };
 
   handleChange = (e) => {
-    this.props.updateMessage("");
+    this.props.updateMessage('');
     this.setState({
       [e.target.name]: e.target.value,
     });
@@ -24,7 +26,7 @@ class SignupForm extends Component {
       await authService.signup(this.state);
       // Let <App> know a user has signed up!
       handleSignupOrLogin();
-      history.push("/");
+      history.push('/');
     } catch (err) {
       updateMessage(err.message);
     }
@@ -38,50 +40,77 @@ class SignupForm extends Component {
   render() {
     const { name, email, password, passwordConf } = this.state;
     return (
-      <div>
-        <h3>Sign Up</h3>
-        <form autoComplete="off" onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            autoComplete="off"
-            id="name"
-            value={name}
-            name="name"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            autoComplete="off"
-            id="email"
-            value={email}
-            name="email"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="email">Email</label>
-          <input
-            type="password"
-            autoComplete="off"
-            id="password"
-            value={password}
-            name="password"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            autoComplete="off"
-            id="confirm"
-            value={passwordConf}
-            name="passwordConf"
-            onChange={this.handleChange}
-          />
-          <label htmlFor="confirm">Confirm Password</label>
-          <button disabled={this.isFormInvalid()}>Sign Up</button>
+      <LoginBox>
+        <h3 style={{ color: 'white' }}>Sign Up:</h3>
+        <Form
+          autoComplete="off"
+          onSubmit={this.handleSubmit}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            padding: '15px',
+            alignItems: 'center',
+          }}
+        >
+          <Form.Group controlledId="formBasicName">
+            <Form.Control
+              type="text"
+              value={name}
+              name="name"
+              onChange={this.handleChange}
+              placeholder="Jane Doe"
+            />
+            <Form.Label style={{ color: 'white' }}>Name</Form.Label>
+          </Form.Group>
+          <Form.Group controlledId="formBasicEmail">
+            <Form.Control
+              type="email"
+              value={email}
+              name="email"
+              onChange={this.handleChange}
+              placeholder="Jane@JaneDoe.com"
+            />
+            <Form.Label style={{ color: 'white' }}>Email:</Form.Label>
+          </Form.Group>
+          <Form.Group controlledId="formBasicPassword">
+            <Form.Control
+              type="password"
+              value={password}
+              name="password"
+              onChange={this.handleChange}
+            />
+            <Form.Label style={{ color: 'white' }}>Password:</Form.Label>
+          </Form.Group>
+          <Form.Group controlledId="formBasicPasswordConf">
+            <Form.Control
+              type="password"
+              value={passwordConf}
+              name="passwordConf"
+              onChange={this.handleChange}
+            />
+            <Form.Label style={{ color: 'white' }}>
+              Confirm Password:
+            </Form.Label>
+          </Form.Group>
+          <Button
+            variant="primary"
+            style={{ width: '100px', margin: '0px auto' }}
+            disabled={this.isFormInvalid()}
+          >
+            Sign Up
+          </Button>
           &nbsp;&nbsp;
-          <Link to="/">Cancel</Link>
-        </form>
-      </div>
+          <Link to="/">
+            <Button
+              variant="danger"
+              style={{ width: '100px', margin: '0px auto' }}
+              type="submit"
+            >
+              Cancel
+            </Button>
+          </Link>
+        </Form>
+      </LoginBox>
     );
   }
 }
